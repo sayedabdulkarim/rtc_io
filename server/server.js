1a48aa8ee87c45ef3a23ab8aac13999f0c22f9ec
Sayed Abdul <sayed_abdul_karim@optum.com>
2024-10-30 00:51:52 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { log } = require("console");
-
 require("dotenv").config();
+
+//routes files
+const authRoutes = require("./routes/authRoutes.js");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
@@ -12,10 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//orutes initialize
+app.use("/api/auth", authRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then((res) => {
-    app.listen(() =>
+    app.listen(PORT, () =>
       console.log(
         "mongoose connected successfully and running on PORT : " + PORT
       )
