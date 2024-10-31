01911498c7009296a11d49254d0a7c05fb29cefa
Sayed Abdul <sayed_abdul_karim@optum.com>
2024-10-31 23:33:37 +0530

    updated

--- client/src/screens/unauth/Login.js
+++ client/src/screens/unauth/Login.js
@@ -4,21 +4,43 @@ import { validateLoginForm } from "../../shared/utils/validators";
 import LoginPageFooter from "../../shared/components/LoginPage/LoginPageFooter";
 import LoginPageHeader from "../../shared/components/LoginPage/LoginPageHeader";
 import LoginPageInputs from "../../shared/components/LoginPage/LoginPageInputs";
+import { useLoginUserMutation } from "../../slices/auth/authApiSlice";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "../../slices/auth/authSlice";
 
 const LoginPage = ({ login }) => {
+  //misc
+  const dispatch = useDispatch();
+  //state
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
+
+  //queries n mutation
+  const [loginUser, { isLoading: loginLoading, error: loginError }] =
+    useLoginUserMutation();
 
   useEffect(() => {
     setIsFormValid(validateLoginForm({ mail, password }));
   }, [mail, password, setIsFormValid]);
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     const userDetails = {
       mail,
       password,
     };
+
+    try {
+      const res = await loginUser(userDetails).unwrap();
+      console.log(res, " resss");
+      localStorage.setItem("jwtToken", res?.userDetails?.token);
+      // handleShowAlert(dispatch, "success", res?.message);
+      dispatch(setCredentials({ ...res?.userDetails }));
+      // navigate("/");
+    } catch (err) {
+      // handleShowAlert(dispatch, "error", err?.data?.message);
+      console.log(err, " errr");
+    }
 
     console.log(userDetails, " we");
   };
